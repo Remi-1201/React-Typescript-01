13bb725981b42258cd806a25cd7be24043ea6fb1
Remi <97021497+Remi-1201@users.noreply.github.com>
2022-07-10 16:43:04 +0900

    2. 型がないせいでバグっているアプリの例

--- README.md
+++ README.md
@@ -3,8 +3,12 @@
 ### Section 10 - React x Typescript（実践編）
 
 1. React × TypeScript の準備
+
    `npm` install などの環境構築
+
    `npx create-react-app my-app --template typescript`
+
+2. 型がないせいでバグっているアプリの例
 
 ### Links
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import axios from "axios";
+import { useState } from "react";
 import "./styles.css";
+import { Todo } from "./Todo";
 
 export default function App() {
+  // 2- <any> = useStateの型
+  const [todos, setTodos] = useState<any>([]);
+
   const onClickFetchData = () => {
     axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      console.log(res);
+      setTodos(res.data);
     });
   };
   return (
     <div className="App">
       <button onClick={onClickFetchData}>Get data</button>
+      {todos.map((todo) => (
+        // 2- 型を指定してあげないと取得した
+        // - userid が undefinedになってしまう
+        <Todo title={todo.title} userid={todo.userid} />
+      ))}
     </div>
   );
 }
